a0fe5cd59eb2b545ca3b563eaa713ebe91d76d08
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-11-17 21:07:46 +0300

    feat:basic header

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import {months, MODULES_BECAME_STANDARD_YEAR } from './host';
-import'./styles/styles.css';
+import {months, MODULES_BECAME_STANDARD_YEAR } from './another';
+import'./styles/reset.css';
+import './styles/header.less';
+import './styles/styles.scss';
+import './styles/default.sass';
 
 console.log(months, MODULES_BECAME_STANDARD_YEAR);
