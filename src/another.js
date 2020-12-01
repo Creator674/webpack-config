0cfe0fe6d8f9180bbfa4429d8706341eb37cd2cd
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-12-01 23:21:06 +0300

    trash: deleted useless code / files

--- src/another.js
+++
@@ -1,2 +0,0 @@
-export let months = ['Jananna', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-export const MODULES_BECAME_STANDARD_YEAR = 2015;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import {months, MODULES_BECAME_STANDARD_YEAR } from './another';
 import './particles';
-import './window';
 import'./styles/reset.css';
 import './styles/sets.less';
 import './styles/body.scss';
 import './styles/default.sass';
 import './styles/text-style.scss';
 
-console.log(months, MODULES_BECAME_STANDARD_YEAR);
 
 window.particlesJS.load('particles-js', 'assets/particles.json');
